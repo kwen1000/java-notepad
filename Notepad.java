01d2f0dd34ff8fb7f8934a8a722cc6de310f95e5
Kevin Wen <preferredcustomer@Preferreds-MacBook-2.local>
2020-01-02 02:23:35 -0800

    all important pack()

--- Notepad.java
+++ Notepad.java
@@ -6,16 +6,25 @@ import javax.swing.text.*;
 
 class Notepad extends JFrame {
     /* Text Editor */
-    private JTextArea editor = new JTextArea(100, 100);
+    private JTextArea editor = new JTextArea(20, 50);
     private JFileChooser dialog = 
 	new JFileChooser(System.getProperty("user.dir"));
     private String fileName = "";
+    private Font font = new Font("Arial", Font.PLAIN, 12);
 
     /* constructor */
     public Notepad() {
+	setTitle(fileName);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	add(new JScrollPane(
+	    editor,
+	    JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+	    JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS
+	    ), BorderLayout.CENTER);
 	JMenuBar menuBar = new JMenuBar();
 	menuBar.add(new JMenu("File"));
 	setJMenuBar(menuBar);
 	setVisible(true);
+	pack();
     }
 }
